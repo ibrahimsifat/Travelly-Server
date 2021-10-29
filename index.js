0031de406e8349f7d68bdc40186885ddb36c416f
ibrahimsifat <ibsifat24@gmail.com>
2021-10-29 14:35:41 +0300

    added index

---
+++ index.js
@@ -0,0 +1,59 @@
+const express=require('express')
+const app=express()
+const port= process.env.PORT || 5000
+require('dotenv').config()
+const { MongoClient } = require('mongodb');
+
+//connect mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1clhw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("tourism");
+      const serviceCollection = database.collection("serviceCollection");
+      // create a document to insert
+      const service = {
+        title: "our service",
+        content: "this is our tourism services ",
+      }
+    //   const result = await serviceCollection.insertOne(service);
+      console.log(`A document was inserted with the _id: ${result.insertedId}`);
+
+
+
+
+
+
+
+
+
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+app.get('/',(req,res)=>{
+    res.send('server is my server is running')
+})
+app.listen(port,()=>{
+    console.log('listening port is ' ,port);
+})
